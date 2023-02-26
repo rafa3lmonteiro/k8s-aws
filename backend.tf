a3d3392a373f19bdfbe93269e32d8cb518791cc2
Rafael Monteiro <rafa3l.monteiro@gmail.com>
2023-02-25 23:13:48 -0300

    update

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         region = "sa-east-1"
-        bucket = "k8s-backend"
+        bucket = "k8s-backend-rafael"
         encrypt = "true"
         key = "terraform.tfstate"    
     } 
