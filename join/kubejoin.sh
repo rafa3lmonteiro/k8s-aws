9e0665b6a3a97475e70ffaec12120d4c846457b9
Rafael Monteiro <rafa3l.monteiro@gmail.com>
2023-03-02 21:28:50 -0300

    updating plabooks

---
+++ join/kubejoin.sh
@@ -0,0 +1,2 @@
+kubeadm join 172.31.7.64:6443 --token my9dq2.3rzvcmzt0dfd5pnd \
+	--discovery-token-ca-cert-hash sha256:dfcefd1d95a73aa94c66d46a87d7a5ca8230c0e69b86006be9a82a0a3700af8a 
--- main.tf
+++ main.tf
@@ -34,7 +34,8 @@ resource "aws_security_group" "k8s-sg" {
 
 resource "aws_instance" "kubernetes-worker" {
   ami       = "ami-04473c3d3be6a927f"
-  instance_type = "t2.micro"
+  #instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name = "k8s-key"
   count = 2
   tags = {
@@ -46,7 +47,8 @@ resource "aws_instance" "kubernetes-worker" {
 
 resource "aws_instance" "kubernetes-master" {
   ami       = "ami-04473c3d3be6a927f"
-  instance_type = "t2.micro"
+  #instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name = "k8s-key"
   count = 1
   tags = {
